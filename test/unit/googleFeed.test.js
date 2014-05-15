f06cdaf7c9a92f3c1eb92045cacc69378230679e
mparaiso <aikah@free.fr>
2014-05-15 13:04:05 +0200

    googleFeed.test Thu May 15 11:04:04 GMT 2014

---
+++ test/unit/googleFeed.test.js
@@ -0,0 +1,35 @@
+/*jslint eqeq:true,node:true,es5:true,white:true,plusplus:true,nomen:true,unparam:true,devel:true,regexp:true */
+/*global angular,jasmine,inject,describe,beforeEach,it,expect*/
+describe('googleFeed',function(){
+    "use strict";
+    beforeEach(function(){
+        var self=this;
+        this.feed=jasmine.createSpyObj('feed',['load','includeHistoricalEntries','setNumEntries']);
+        this.google={
+            feeds:{
+                findFeeds:jasmine.createSpy('findFeeds'),
+                Feed:function(){
+                    return self.feed;
+                }
+            }
+        };
+        angular.module('test',['googleFeed'],function(feedFinderProvider){
+            feedFinderProvider.setGoogle(self.google);
+            feedFinderProvider.setNumEntries(50);
+        });
+        module('test');
+        inject(function($injector,$timeout) {
+            self.$injector=$injector;
+            self.$timeout=$timeout;
+        });
+    });
+    describe('feedFinder',function(){
+        beforeEach(function(){
+            this.feedFinder=this.$injector.get('feedFinder');
+        });
+        it('findFeedByUrl',function(done){
+            this.feed.load.and.callFake(done);
+            this.feedFinder.findFeedByUrl("foo"); 
+        });
+    });
+});
