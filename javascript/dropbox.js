0dfc8fca639cfceb229c8fd600cb5809fdce07ce
mparaiso <aikah@free.fr>
2014-05-30 14:40:03 +0200

    more tests Fri May 30 12:40:02 GMT 2014

--- javascript/dropbox.js
+++ javascript/dropbox.js
@@ -4,59 +4,17 @@
  * @copyright 2014 mparaiso <mparaiso@online.fr>
  * @license GPL
  */
+var fluxreader=fluxreader||{};
 (function () {
     "use strict";
-    /**
-     * angular dropBox module
-     * MANAGE DROPBOX API
-     * @dependencies dropbox-datastores-1.0-latest.js
-     */
-    angular.module('dropbox', []) 
-        .constant('DROPBOX_APIKEY', 'jze8pzfye506das' /* override with your api key */ )
-        .factory('client', function (DROPBOX_APIKEY){
-            return new Dropbox.Client({
-                key: DROPBOX_APIKEY
-            });
-        })
-        .provider('dropboxClient', function () {
-            /**
-             * DROPBOX API
-             * service configuration
-             */
-            return {
-                /* service */
-                $get: function ($timeout,client) {
-                    return {
-                        authenticate:function(callback){
-                            callback=callback||function(){};
-                            client.authenticate({
-                                interactive: false
-                            },callback);
-                        },
-                        /* sign in */
-                        signIn: function () {
-                            client.authenticate();
-                        },
-                        /* sign out */
-                        signOut: function (callback) {
-                            client.signOut(callback);
-                        },
-                        isAuthenticated: function () {
-                            return client.isAuthenticated();
-                        },
-                        getDatastoreManager: function () {
-                            return client.getDatastoreManager();
-                        },
-                        /* get account info */
-                        getAccountInfo: function (callback) {
-                            client.getAccountInfo({
-                                httpCache: true
-                            }, callback);
-                            /* @link https://www.dropbox.com/developers/datastore/docs/js#Dropbox.Client.getAccountInfo */
 
-                        }
-                    };
-                }
-            };
-        });
+    /**
+    * angular dropBox module
+    * MANAGE DROPBOX API
+    * @dependencies dropbox-datastores-1.0-latest.js
+    */
+    angular.module('dropbox', []) 
+    .constant('DROPBOX_APIKEY', 'jze8pzfye506das' /* override with your api key */ )
+    .service('client', ['DROPBOX_APIKEY',fluxreader.Client])
+    .service('dropboxClient',fluxreader.DropboxClient );                        /* sign in */
 }());
